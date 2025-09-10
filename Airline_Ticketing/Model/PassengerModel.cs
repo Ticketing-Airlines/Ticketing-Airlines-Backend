521bc8d7222d4fd7deece17ca2fc4576416dabd4
Karl <karllastimoso454@gmail.com>
2025-09-11 07:05:03 +0800

    Add project files.

---
+++ Airline_Ticketing/Model/PassengerModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Airline_Ticketing.Model
+{
+    public class PassengerModel
+    {
+        public int PassengerID { get; set; }
+        public String FirstName { get; set; }
+
+        public String LastName { get; set; }
+
+        public String Email { get; set; }
+
+        public int PassPortNumber { get; set; }
+
+    }
+}
