b3635e38ae5629f001d614787df9a28f20ba7b96
Kaizzz <karllastimoso454@gmail.com>
2025-09-11 17:44:49 +0800

    feat(models): add booking and user models

---
+++ Airline_Ticketing/Model/UserModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Airline_Ticketing.Model
+{
+    public class UserModel
+    {
+
+        public int UserID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public  string Password { get; set; }
+
+        public int Phone { get; set; }
+
+        public DateTime  CreatedAt { get; set; }
+    }
+}
