535a3c1a0256c09acb7a98bb1933bd27ae00fb56
Kaizzz <karllastimoso454@gmail.com>
2025-09-11 23:32:26 +0800

    feat(data): add ApplicationDbContext for EF Core integration

---
+++ Airline_Ticketing/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Airline_Ticketing.Model;
+using Microsoft.EntityFrameworkCore;    
+
+namespace Airline_Ticketing.Data
+
+    {
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<UserModel> Users { get; set; }
+        public DbSet<FlightModel> Flights { get; set; }
+        public DbSet<BookingModel> Bookings { get; set; }
+        public DbSet<PassengerModel> Passengers { get; set; }
+    }
+}
