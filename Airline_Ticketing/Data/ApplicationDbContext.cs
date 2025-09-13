6d5520a5115c3ad8b7ac8cb0be09e7795f31933c
Kaizzz <karllastimoso454@gmail.com>
2025-09-13 12:03:22 +0800

    feat(data): add dbsets for new entities

--- Airline_Ticketing/Data/ApplicationDbContext.cs
+++ Airline_Ticketing/Data/ApplicationDbContext.cs
@@ -14,5 +14,30 @@ namespace Airline_Ticketing.Data
         public DbSet<FlightModel> Flights { get; set; }
         public DbSet<BookingModel> Bookings { get; set; }
         public DbSet<PassengerModel> Passengers { get; set; }
+
+        public DbSet<AdminModel> Admins { get; set; }
+
+        public DbSet<BookingPassengerModel> BookingPassengers { get; set; }
+
+       public DbSet<AircraftModel> Aircrafts { get; set; }
+
+        public DbSet<AirportModel> Airports { get; set; }
+
+        public DbSet<PaymentModel> Payments { get; set; }
+
+        public DbSet<TicketsModel> Tickets { get; set; }
+
+        public DbSet<SeatLayoutModel> SeatLayouts { get; set; }
+
+        public DbSet<AirlineModel> Airlines { get; set; }
+
+        public DbSet<FlightPricesModel> FlightPrices { get; set; }
+
+        public DbSet<FlightSeatModel> FlightSeats { get; set; }
+
+
+
+
+
     }
 }
