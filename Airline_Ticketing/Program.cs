1c9241c0c7acfcfd3bb106da1b8208a713201582
Kaizzz <karllastimoso454@gmail.com>
2025-09-13 23:40:32 +0800

    chore(config): add ef core setup and swagger as comment code for now

--- Airline_Ticketing/Program.cs
+++ Airline_Ticketing/Program.cs
@@ -1,11 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using Airline_Ticketing.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//commented since not connected to a database yet
+
+//var connectionString = builder.Configuration.GetConnectionString("DefaultConnection"); 
+
+//builder.Services.AddDbContext<ApplicationDbContext>(options =>
+//    options.UseSqlServer(connectionString));
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();   
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
