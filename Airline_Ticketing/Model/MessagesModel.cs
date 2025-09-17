3aca3e2c480c2f0196b65c7dd52f27ed32fc878e
Kaizzz <karllastimoso454@gmail.com>
2025-09-17 19:51:18 +0800

    feat: add messages model for chat functionality

---
+++ Airline_Ticketing/Model/MessagesModel.cs
@@ -0,0 +1,27 @@
+﻿using Airline_Ticketing.Enums;
+
+namespace Airline_Ticketing.Model
+
+   
+{
+    public class MessagesModel
+    {
+
+        public int MessageID { get; set; }
+
+        public int ChatID { get; set; }
+
+        public SenderType SenderType { get; set; }
+
+        public string MessageText { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public MessagesModel()
+        {
+            CreatedAt = DateTime.UtcNow;
+        }
+
+
+    }
+}
