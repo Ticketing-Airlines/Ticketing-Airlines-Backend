bcfc88ba3eeffcdf9b1fc3276fd53a5faf638b52
Kaizzz <karllastimoso454@gmail.com>
2025-10-13 23:45:12 +0800

    feat: add passenger request dtos with validation

---
+++ Airline_Ticketing/DTOs/Request/CreatePassengerRequest.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace Airline_Ticketing.DTOs.Request
+{
+    public class CreatePassengerRequest
+    {
+       
+        public string Name { get; set; }
+
+        public DateOnly DateOfBirth { get; set; }
+
+        public string? PassportNumber { get; set; }
+
+        public string? Nationality { get; set; }
+    }
+}
