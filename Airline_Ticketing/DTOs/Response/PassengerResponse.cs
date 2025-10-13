b766570125ea132bd78aa88406a0cc1ff4404898
Kaizzz <karllastimoso454@gmail.com>
2025-10-14 00:03:46 +0800

    feat: add passenger response for passenger details

---
+++ Airline_Ticketing/DTOs/Response/PassengerResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace Airline_Ticketing.DTOs.Response
+{
+    public class PassengerResponse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateOnly DateOfBirth { get; set; }
+
+        public string? Nationality { get; set; }
+
+    }
+}
