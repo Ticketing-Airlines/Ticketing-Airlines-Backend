0419cd1bd22bd8e8de6a5501db9581a84d97eea2
Kaizzz <karllastimoso454@gmail.com>
2025-10-28 05:33:51 +0800

    feat(migration): refactor user name fields and add booking relationships

---
+++ Airline_Ticketing/Migrations/20251027161453_RefactorBookingFields.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Airline_Ticketing.Migrations
+{
+    /// <inheritdoc />
+    public partial class RefactorBookingFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Users",
+                newName: "LastName");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "MiddleName",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "FlightID",
+                table: "Bookings",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserID",
+                table: "Bookings",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "MiddleName",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "FlightID",
+                table: "Bookings");
+
+            migrationBuilder.DropColumn(
+                name: "UserID",
+                table: "Bookings");
+
+            migrationBuilder.RenameColumn(
+                name: "LastName",
+                table: "Users",
+                newName: "Name");
+        }
+    }
+}
