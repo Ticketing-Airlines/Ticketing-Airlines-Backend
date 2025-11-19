b5d60a178cd41d12b67d7c7bd7203bcc676ee63c
Kaizzz <karllastimoso454@gmail.com>
2025-11-19 14:37:31 +0800

    feat: add ITicketRepository interface for async ticket ops

---
+++ Airline_Ticketing/IRepository/ITicketRepository.cs
@@ -0,0 +1,18 @@
+﻿using Airline_Ticketing.Model;
+using System.Runtime.CompilerServices;
+
+namespace Airline_Ticketing.IRepository
+{
+    public interface ITicketRepository
+    {
+        Task<IEnumerable<Tickets>> GetAllAsync();
+        Task<Tickets?> GetByIdAsync(int id);
+
+        Task<Tickets> AddAsync(Tickets ticket);
+
+        Task<Tickets> UpdateAsync(Tickets ticket);
+
+        Task<bool> DeleteAsync(int id);
+     
+     }
+}
