ac23121446ecaae741a6b8249971cda68fa59996
Kaizzz <karllastimoso454@gmail.com>
2026-04-09 00:16:01 +0800

    feat: add BookingFlight join model

---
+++ Airline1/Models/BookingFlight.cs
@@ -0,0 +1,15 @@
+﻿namespace Airline1.Models
+{
+    public class BookingFlight
+    {
+        public int Id { get; set; }
+
+        public int BookingId { get; set; }
+
+        public Booking? Booking { get; set; }
+
+        public int FlightId { get; set; }
+
+        public Flight? Flight { get; set; } 
+    }
+}
