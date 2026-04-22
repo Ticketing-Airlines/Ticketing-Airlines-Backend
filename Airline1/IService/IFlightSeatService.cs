73106164df5a274fbbfa42e8e9ea313e7966a486
Kaizzz <karllastimoso454@gmail.com>
2026-04-22 19:08:51 +0800

    refactor(flight-seat): update interface to use guid for seat ids

--- Airline1/IService/IFlightSeatService.cs
+++ Airline1/IService/IFlightSeatService.cs
@@ -1,4 +1,4 @@
-﻿using Airline1.Dtos.Requests;
+using Airline1.Dtos.Requests;
 using Airline1.Dtos.Responses;
 
 namespace Airline1.IService
@@ -6,10 +6,10 @@ namespace Airline1.IService
     public interface IFlightSeatService
     {
         Task InitializeSeatsForFlightAsync(int flightId);
-        Task<FlightSeatResponse> ReserveSeatAsync(int flightSeatId, ReserveFlightSeatRequest request);
-        Task<FlightSeatResponse> AssignSeatAsync(int flightSeatId, AssignFlightSeatRequest request);
-        Task<FlightSeatResponse> BlockSeatAsync(int flightSeatId, BlockFlightSeatRequest request);
+        Task<FlightSeatResponse> ReserveSeatAsync(Guid flightSeatId, ReserveFlightSeatRequest request);
+        Task<FlightSeatResponse> AssignSeatAsync(Guid flightSeatId, AssignFlightSeatRequest request);
+        Task<FlightSeatResponse> BlockSeatAsync(Guid flightSeatId, BlockFlightSeatRequest request);
         Task<IEnumerable<FlightSeatResponse>> GetByFlightAsync(int flightId);
-        Task<FlightSeatResponse> GetByIdAsync(int id);
+        Task<FlightSeatResponse> GetByIdAsync(Guid id);
     }
 }
